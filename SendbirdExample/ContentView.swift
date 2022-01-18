26abcdafbabac1437bc521763a95f3b4d79b6fa2
Ernest Hong <hsh3592@gmail.com>
2022-01-18 15:14:50 +0900

    Create Project

---
+++ SendbirdExample/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SendbirdExample
+//
+//  Created by Ernest Hong on 2022/01/18.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SendbirdExample/SendbirdExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SendbirdExampleApp.swift
+//  SendbirdExample
+//
+//  Created by Ernest Hong on 2022/01/18.
+//
+
+import SwiftUI
+
+@main
+struct SendbirdExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
